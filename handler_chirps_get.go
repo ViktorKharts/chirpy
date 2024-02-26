6c3c7c09a5d0160f7adf48623ec1feaeae772373
viktor.k <viktor.kharts@gmail.com>
2024-02-26 10:55:15 +0100

    add get chirps author id filter param

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"sort"
 
 	"github.com/ViktorKharts/chirpy/internal/database"
 )
 
 func (c *apiConfig) getChirpsHandler(w http.ResponseWriter, r *http.Request) {
+	userID := r.URL.Query().Get("author_id")
+
 	dbChirps, err := c.DB.GetChirps()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
@@ -19,10 +22,28 @@ func (c *apiConfig) getChirpsHandler(w http.ResponseWriter, r *http.Request) {
 		chirps = append(chirps, c)
 	}
 
+	if userID != "" {
+		userIDI, err := strconv.Atoi(userID) 	
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		chirps = filterChirpsByUserID(chirps, userIDI)
+	}
+
 	sort.Slice(chirps, func(i, j int) bool {
 		return chirps[i].ID < chirps[j].ID
 	})
 
 	respondWithJson(w, http.StatusOK, chirps)
+}
+
+func filterChirpsByUserID(c []database.Chirp, userID int) (r []database.Chirp) {
+	for _, chirp := range c {
+		if chirp.AuthorID == userID {
+			r = append(r, chirp)
+		}
+	}
+	return r
 }
 
